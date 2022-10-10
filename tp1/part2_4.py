ca35f66d21ed493d827ee8326a7292bfa0b98c00
Ingargiola Emma <ingargio@insa-11277>
2022-10-10 12:18:23 +0200

    add part3

--- tp1/part2_4.py
+++ tp1/part2_4.py
@@ -6,7 +6,7 @@ Created on Wed Oct  5 11:58:39 2022
 @author: ingargio
 """
 
-from sklearn import metrics
+from sklearn import metrics, cluster
 import kmedoids
 from scipy.io import arff
 import time 
@@ -14,25 +14,89 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import manhattan_distances
+from sklearn.metrics.cluster import rand_score
 
-path = '../../artificial/'
+
+
+def search_cluster(metric, databrut, k_ref) :
+    datanp =  [[x[0],x[1]] for x in databrut[0]]
+    
+    f0 = [f[0] for f in datanp]
+    f1 = [f[1] for f in datanp]
+    plt.scatter(f0 , f1,s=8) 
+    plt.show()
+    
+    
+    
+    if metric == "silhouette" :
+        coefs = [99.,99.]
+        coefs_man = [99.,99.]
+        coefs_kmeans = [99.,99.]
+    elif metric == "db" :
+        coefs = [99.,99.]
+        
+    for k in range(2,15):
+        tps1 = time . time ()
+        distmatrix = euclidean_distances ( datanp )
+        fp = kmedoids . fasterpam ( distmatrix , k )
+        tps2 = time . time ()
+        iter_kmed = fp . n_iter
+        labels= fp . labels
+        
+        distmatrix_man = manhattan_distances ( datanp )
+        fp_man = kmedoids . fasterpam ( distmatrix_man , k )
+        iter_kmed_man = fp_man . n_iter
+        labels_man= fp_man . labels
+        
+        model = cluster.KMeans(n_clusters=k, init='k-means++') 
+        model.fit(datanp) 
+        labels_kmeans = model.labels_ 
+       
+        if metric == "silhouette" :
+            coefs.append(abs(metrics.silhouette_score(datanp, labels)-1))
+            coefs_man.append(abs(metrics.silhouette_score(datanp, labels)-1))
+            coefs_kmeans.append(abs(metrics.silhouette_score(datanp, labels_kmeans)-1))
+        elif metric == "db" :
+            coefs.append(metrics.davies_bouldin_score(datanp, labels))
+            
+        plt.scatter(f0, f1, c=labels, s=8)
+        plt.title("Donnees apres clusturing Kmedoids")
+        plt.show()
+        print("nb clusters=", k, " , nb iter =" , iter_kmed, ", ...... runtime= ", round((tps2 -tps1)*1000,2),"ms" )
+
+        
+    print(coefs)
+    
+    if metric=="silhouette":
+        print("rand_score euclidian vs manhattan ",rand_score(labels, labels_man))
+        print("rand_score ",rand_score(labels, labels_kmeans))
+        print("mutual info ",metrics.mutual_info_score(labels, labels_kmeans))
+    plt.plot(coefs)
+    plt.show()
+    
+    for i in range(len(coefs)) :
+        
+        if metric == "silhouette" :
+            if coefs[i]==min(coefs):
+                print("silhouette Mectric la plus proche de 1 : ",min(coefs), ", atteint pour nb de cluster = ",i, "k_ref = ", k_ref)
+          
+        elif metric == "db" :
+            if coefs[i]==min(coefs):
+                print("db Minimum de notre mectric : ",min(coefs), ", atteint pour nb de cluster = ",i, "k_ref = ", k_ref)
+   
+    #fait varier k en calculant à chaque fois avec les métriques d'évaluation, si on a une bonne valeur alors on a le bon nb de clusters (k)
+    # a refaire sur pls jeu de données
+
+path = '../artificial/'
 databrut = arff.loadarff(open(path+"2d-10c.arff",'r'))
+databrut2 = arff.loadarff(open(path+"xclara.arff",'r'))
+databrut3 = arff.loadarff(open(path+"sizes4.arff",'r'))
 
-datanp =  [[x[0],x[1]] for x in databrut[0]]
- 
-f0 = [f[0] for f in datanp]
-f1 = [f[1] for f in datanp]
+search_cluster("silhouette",databrut, 9)
+search_cluster("db",databrut, 9)
+search_cluster("silhouette",databrut2, 3)
+search_cluster("db",databrut2, 3)
+search_cluster("silhouette",databrut3,4)
+search_cluster("db",databrut3,4)
 
-tp1= time.time()
-k=3
-distmatrix= euclidean_distances(datanp)
-fp= kmedoids.fasterpam(distmatrix,k)
-tps2= time.time()
-iter_kmed= fp.n_iter
-labels_kmed= fp.labels
-print("Loss with FasterPAM:", fp.loss)
-
-plt.scatter(f0, f1, c=labels_kmed, s=8)
-plt.title("Donnees apres clusturing Kmedoids")
-plt.show()
-print("nb clusters=", k, " , nb iter =" , iter_kmed, ", ...... runtime= ", round((tps2 -tp1)*1000,2),"ms" )
+# 2.4 : silhouette sur kmedoids ne fait pas les bons calculs 
--- tp1/part3_1.py
+++ tp1/part3_1.py
@@ -1,62 +1,137 @@
 import scipy.cluster.hierarchy as shc 
 import scipy.cluster
-from sklearn import metrics
+from sklearn import metrics, cluster
 import kmedoids
 from scipy.io import arff
 import time 
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-path = '../../artificial/'
+def search_dist(method,d_start,d_end, databrut, k_ref) :
+    datanp =  [[x[0],x[1]] for x in databrut[0]]
+    
+    f0 = [f[0] for f in datanp]
+    f1 = [f[1] for f in datanp]
+    
+    # Le code ci-dessous permet d’afficher un dendrogramme (il y a d’autres possibilités ...) avec la méthode d’agglomération de clusters single
+
+    # Donnees dans datanp 
+    print ("Dendrogramme ", method," donnees initiales ") 
+
+    linked_mat = shc.linkage(datanp , method) 
+
+
+    plt.figure( figsize =(12, 12)) 
+    shc.dendrogram(linked_mat , orientation='top' , distance_sort='descending' , show_leaf_counts=False) 
+    plt.show()
+    
+    coefs = []
+    cluster_dist = []
+    dist = []
+    
+    for d in range(d_start,d_end):
+        
+        # Le code suivant permet de déterminer un clustering hiérarchique en utilisant soit une limite sur le seuil de distance soit un nombre de clusters.
+
+
+        # set distance threshold (0 ensures we compute thge full tree)
+        tps1= time.time()
+        model= cluster.AgglomerativeClustering(distance_threshold= d, linkage=method, n_clusters=None)
+        model=model.fit(datanp)
+        tps2=time.time()
+
+        labels= model.labels_
+        k= model.n_clusters_
+        cluster_dist.append(k)
+        leaves=model.n_leaves_
+
+        # affichage clustering
+        plt.scatter(f0, f1, c=labels, s=8)
+        plt.title("Résultat du clustering")
+        plt.show()
+        print(d," nb clusters =",k,", nb feuilles = ", leaves, ", runtime= ", round((tps2-tps1)*1000,2), "ms")
+
+        dist.append(d)
+        coefs.append(abs(metrics.silhouette_score(datanp, labels)-1)) 
+        
+        
+    print(coefs)
+    plt.plot(coefs)
+    plt.show()
+    
+    for i in range(len(coefs)) :
+        if coefs[i]==min(coefs):
+            print(method, " silhouette Mectric la plus proche de 1 : ",min(coefs), ", atteint pour nb de cluster = ",cluster_dist[i]," distance ",dist[i],"k_ref = ", k_ref)
+
+def search_cluster(method,databrut, k_ref) :
+    datanp =  [[x[0],x[1]] for x in databrut[0]]
+    
+    f0 = [f[0] for f in datanp]
+    f1 = [f[1] for f in datanp]
+    
+    # Le code ci-dessous permet d’afficher un dendrogramme (il y a d’autres possibilités ...) avec la méthode d’agglomération de clusters single
+
+    # Donnees dans datanp 
+    print ("Dendrogramme ", method," donnees initiales ") 
+
+    linked_mat = shc.linkage(datanp , method) 
+
+
+    plt.figure( figsize =(12, 12)) 
+    shc.dendrogram(linked_mat , orientation='top' , distance_sort='descending' , show_leaf_counts=False) 
+    plt.show()
+    
+    coefs = [99.0,99.0]
+    
+    for k in range(2,15):
+        
+    
+        tps1=time.time()
+        model= cluster.AgglomerativeClustering(linkage=method, n_clusters=k)
+        model= model.fit(datanp)
+        tps2=time.time()
+
+        labels= model.labels_
+        kres= model.n_clusters_
+        leaves=model.n_leaves_
+        
+        # affichage clustering
+        plt.scatter(f0, f1, c=labels, s=8)
+        plt.title("Résultat du clustering")
+        plt.show()
+        
+        coefs.append(abs(metrics.silhouette_score(datanp, labels)-1)) 
+        
+        
+        
+    print(coefs)
+    plt.plot(coefs)
+    plt.show()
+    
+    for i in range(len(coefs)) :
+        if coefs[i]==min(coefs):
+            print(method, " silhouette Mectric la plus proche de 1 : ",min(coefs), ", atteint pour nb de cluster = ",i,"k_ref = ", k_ref)
+
+
+path = '../artificial/'
 databrut = arff.loadarff(open(path+"2d-10c.arff",'r'))
+databrut2 = arff.loadarff(open(path+"xclara.arff",'r'))
+databrut3 = arff.loadarff(open(path+"sizes4.arff",'r'))
+#dist varie
+# search_dist("average",10,30,databrut, 9)
+# search_dist("average",30,60,databrut2, 3)
+# search_dist("average",4,9,databrut3,4)
 
-datanp =  [[x[0],x[1]] for x in databrut[0]]
- 
-f0 = [f[0] for f in datanp]
-f1 = [f[1] for f in datanp]
+#cluster varie
+# search_cluster("average",databrut, 9)
+# search_cluster("average",databrut2, 3)
+# search_cluster("average",databrut3, 4)
 
+#method varie
+search_dist("single",1,10,databrut2,3) 
+search_dist("average",30,60,databrut2,3)
+search_dist("complete",30,60,databrut2,3)
+search_dist("ward",30,60,databrut2,3)
 
-# Le code ci-dessous permet d’afficher un dendrogramme (il y a d’autres possibilités ...) avec la méthode d’agglomération de clusters single
-
-# Donnees dans datanp 
-print ("Dendrogramme 'single' donnees initiales ") 
-
-linked_mat = shc.linkage(datanp , 'single') 
-
-
-plt.figure( figsize =(12, 12)) 
-shc.dendrogram(linked_mat , orientation='top' , distance_sort='descending' , show_leaf_counts=False) 
-plt.show()
-
-
-# Le code suivant permet de déterminer un clustering hiérarchique en utilisant soit une limite sur le seuil de distance soit un nombre de clusters.
-
-
-# set distance threshold (0 ensures we compute thge full tree)
-tps1= time.time()
-model= scipy.cluster.AgglomerativeClustering(distance_threshold= 10, linkage='single', n_clusters=None)
-model=model.fir(datanp)
-tps2=time.time()
-
-labels= model.labels_
-k= model.c_clusters_
-leaves=model.n_leaves_
-
-# affichage clustering
-plt.scatter(f0, f1, c=labels, s=8)
-plt.title("Résultat du clustering")
-plt.show()
-print("nb clusters =",k,", nb feuilles = ", leaves, ", runtime= ", round((tps2-tps1)*1000,2), "ms")
-
-# set the number of clusters 
-k=4
-tps1=time.time()
-model= scipy.cluster.AgglomerativeClustering(linkage='single', n_clusters=k)
-model= model.fit(datanp)
-tps2=time.time()
-
-labels= model.labels_
-k= model.c_clusters_
-leaves=model.n_leaves_
 
 
